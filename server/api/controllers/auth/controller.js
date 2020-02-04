82c9224fda7ed22a7bc6b2ca596b544243209ec4
phuongnam7899 <phuongnam7899@gmail.com>
2020-02-04 20:06:19 +0700

    test api

---
+++ server/api/controllers/auth/controller.js
@@ -0,0 +1,9 @@
+
+
+
+export class Controller {
+    getOne(req, res) {
+        res.send("hello")
+    }
+}
+export default new Controller();
--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,10 @@
 const bodyParser = require("../node_modules/body-parser");
 const morgan = require('../node_modules/morgan');
 const jwt = require('../node_modules/jsonwebtoken');
+import authRouter from "./api/controllers/auth/router"
 
 export default function routes(app) {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(morgan('dev'));
+  app.use("/api/auth", authRouter)
 }
