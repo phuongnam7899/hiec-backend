3b6a3b0df56c125f1822c944865018f232aa471d
phuongnam7899 <phuongnam7899@gmail.com>
2020-02-21 21:58:22 +0700

    modify news api

--- server/api/models/news.js
+++ server/api/models/news.js
@@ -9,17 +9,6 @@ const NewsSchema = new Schema({
     postTime : {type : String, require : true},
     title : {type : String},
     content : {type : String, require : true},
-    viewer : [String],
-    clap : [String],
-    comments : [{
-        user : {type : mongoose.Types.ObjectId, ref : "user", require : true},
-        content : {type : String},
-        time : {type : String},
-        replies : [{
-            user : {type : mongoose.Types.ObjectId, ref : "user", require : true},
-            content : {type : String},
-            time : {type : String, require : true}
-        }]
-    }]
+    viewer : [String]
 })
 export default Model("new",NewsSchema);
