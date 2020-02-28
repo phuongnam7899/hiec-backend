69f3daa89413e7655c09763d766b2bcc33d76bb5
phuongnam7899 <phuongnam7899@gmail.com>
2020-02-28 14:32:04 +0700

    nothing

--- server/common/server.js
+++ server/common/server.js
@@ -4,6 +4,7 @@ import * as bodyParser from 'body-parser';
 import * as http from 'http';
 import * as os from 'os';
 import cookieParser from 'cookie-parser';
+const serverless = require("serverless-http")
 const mongoose = require("mongoose");
 
 import swaggerify from './swagger';
@@ -13,7 +14,7 @@ import l from './logger';
 //npm i cors nhé
 const cors = require('cors');
 
-const app = new Express();
+const app =  new Express();
 
 export default class ExpressServer {
   constructor() {
@@ -38,8 +39,8 @@ export default class ExpressServer {
         }
       );
     app.set('appPath', `${root}client`);
-    app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || '100kb' }));
-    app.use(bodyParser.urlencoded({ extended: true, limit: process.env.REQUEST_LIMIT || '100kb' }));
+    app.use(bodyParser.json({ limit: '500kb' }));
+    app.use(bodyParser.urlencoded({ extended: true, limit:'500kb' }));
     //nhớ thêm dòng này để link vs frontend
     app.use(cors({
       origin: ["http://localhost:3000","https://xtutor.herokuapp.com"],
@@ -59,6 +60,6 @@ export default class ExpressServer {
   listen(port = process.env.PORT) {
     const welcome = p => () => l.info(`up and running in ${process.env.NODE_ENV || 'development'} @: ${os.hostname()} on port: ${p}}`);
     http.createServer(app).listen(port, welcome(port));
-    return app;
+    return serverless(app);
   }
 }
