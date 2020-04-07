87927f1cea6c52957d3523abbdef0b5a061fe62b
phuongnam7899 <phuongnam7899@gmail.com>
2020-04-08 00:17:23 +0700

    change default avt

--- server/api/models/user.js
+++ server/api/models/user.js
@@ -18,7 +18,7 @@ const UserSchema = new Schema({
             describe : {} // jobTitle - companyName or school - major
         },
         address : {type : String},
-        avatar : {type : String, default : "https://i.pinimg.com/236x/8c/87/6e/8c876ed0976c074a8e7664fc21422fb0.jpg"}
+        avatar : {type : String, default : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQcVTlEXUHIyBJGKGKRcdKVOYhb22zka6_CZ0VwJ4z2c7wSlWw6&usqp=CAU"}
     }
 });
 export default Model("user",UserSchema);
